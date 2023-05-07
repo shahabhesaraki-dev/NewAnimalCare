ea322c651996b6c4ffc8d25c83b47aa737ff75d5
Shahab Hesaraki <shahabhes@gmail.com>
2023-05-07 12:04:25 -0400

    NEW PUSH

--- server/index.js
+++ server/index.js
@@ -26,7 +26,7 @@ const {
   getMessagesByConversationId,
 } = require("./handler");
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 const app = express();
 app.use(cors());
